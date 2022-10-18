b5c9828cece91e413a1d9294ab0b4391c11303f2
Zainab-Alzubair <zainab.a.elzubair@gmail.com>
2022-10-18 23:27:09 +0300

    Add mobile menu

---
+++ nav-menu.js
@@ -0,0 +1,17 @@
+const cross = document.querySelector('#close-button');
+const navWBtn = document.getElementsByClassName('nav-w-btn');
+const humberger = document.querySelector('#humberger');
+
+humberger.addEventListener('click', () => {
+  document.getElementById('nav-wrap').style.left = 0;
+});
+
+cross.addEventListener('click', () => {
+  document.getElementById('nav-wrap').style.left = '100%';
+});
+
+for (let i = 0; i < navWBtn.length; i += 1) {
+  navWBtn[i].addEventListener('click', () => {
+    document.getElementById('nav-wrap').style.left = '100%';
+  });
+}
