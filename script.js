2d9462a28b4349713ab1aa74f086a76b8cb20c05
Zainab-Alzubair <zainab.a.elzubair@gmail.com>
2022-10-20 11:55:21 +0300

    Fix linters errors

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 // dynamic preview-cards
 const cards = [{
- 
+
   img: '/img/speaker1.png',
-  name: "Angela Mark",
-  title: "full-stack developer",
+  name: 'Angela Mark',
+  title: 'full-stack developer',
   des: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. ',
- 
+
 },
 {
   img: '/img/speaker2.png',
-  name: "Steph Portilla",
-  title: "Web application developer",
+  name: 'Steph Portilla',
+  title: 'Web application developer',
   des: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. ',
 },
 {
   img: '/img/speaker3.png',
-  name: "Jose Steele",
-  title: "Front end developer",
+  name: 'Jose Steele',
+  title: 'Front end developer',
   des: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. ',
 },
 {
   img: '/img/speaker4.png',
-  name: "Colt Jonas",
-  title: "Back end developer",
+  name: 'Colt Jonas',
+  title: 'Back end developer',
   des: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. ',
 },
 {
   img: '/img/speaker5.png',
-  name: "Kyle Tim",
-  title: "Web Designer ",
+  name: 'Kyle Tim',
+  title: 'Web Designer ',
   des: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. ',
 },
 {
   img: '/img/speaker6.png',
-  name: "Jonas Deo",
-  title: "Game developer",
+  name: 'Jonas Deo',
+  title: 'Game developer',
   des: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. ',
 },
 
@@ -42,9 +42,8 @@ const cards = [{
 
 function displayCards() {
   let result = '';
- 
-  cards.forEach((card) =>{
-   
+
+  cards.forEach((card) => {
     result += `
     <div class="instructor-card">
     <div>
@@ -59,7 +58,7 @@ function displayCards() {
   </div>`;
   });
 
-document.querySelector('.even-columns-instructor').innerHTML = result;
+  document.querySelector('.even-columns-instructor').innerHTML = result;
 }
 
 displayCards();
